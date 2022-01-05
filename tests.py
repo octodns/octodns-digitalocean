f0233ed836e7c6a5dccd85318dea79b30b86ba4c
Ross McFarland <rwmcfa1@gmail.com>
2022-01-05 09:28:55 -0800

    Import skeleton

---
+++ tests.py
@@ -0,0 +1,12 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_digitalocean import DigitalOceanProvider
+
+
+class TestDigitalOceanProvider(TestCase):
+    # TODO: test provider
+    pass
